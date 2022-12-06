4c4878e56e51e60ebaa0fc220fe96b42267d7823
vadim <>
2022-12-06 22:27:36 +0100

    first commit

---
+++ test_f.py
@@ -0,0 +1,24 @@
+import time
+
+from seleniumbase import BaseCase
+import pytest
+from behave import step
+from pytest_bdd import scenarios, given, when, then
+
+
+class test_ca(BaseCase):
+    login_field = "//*[@id='email']"
+    password_field = "//*[@id='password']"
+
+    def test_swag_labs(self):
+        self.open("https://aimaidhelp.com/login")
+        self.type(self.login_field, text='vburmich@apro.ai')
+        self.type(self.password_field, text='1111')
+        self.click("//*[text()='Log In']")
+        a = 'awdawd'
+        self.assert_title('AI Maid Help')
+        print("test")
+
+
+
+
